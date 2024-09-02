24c4d2a068d65524ae43fd3f0ad3af5b5b7109a4
sibi-7 <sibi772001@gmail.com>
2024-09-02 21:25:55 +0530

    Add : portfolio js file

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    document.getElementById('learnMore').addEventListener('click', function() {
+        document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    });
+});
